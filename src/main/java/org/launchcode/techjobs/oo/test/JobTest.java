8c54eae95b786e3487fdbc060b22d14080866317
Mattx2k1 <44537080+Mattx2k1@users.noreply.github.com>
2021-10-25 15:31:21 -0400

    part 3 of test full constructor

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -4,7 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import org.launchcode.techjobs.oo.Job;
+import org.launchcode.techjobs.oo.*;
 
 import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertTrue;
@@ -16,12 +16,19 @@ import static junit.framework.TestCase.assertTrue;
 public class JobTest {
     Job testJob1 = new Job();
     Job testJob2 = new Job();
+    Job testJob3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
 
 
     @Test
     public void testSettingJobId() {
         assertEquals(testJob1.getId(), testJob2.getId(), 1);
-        System.out.println("IDs match"); // my own identifier test has passed; (It did!)
+            System.out.println("IDs match"); // my own identifier test has passed; (Actually this would print anyways I believe =(..)
+
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        assertTrue(testJob3.getName(), Boolean.parseBoolean("Product tester"));
 
     }
 

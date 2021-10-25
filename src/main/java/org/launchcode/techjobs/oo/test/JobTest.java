69914add316fe5aad4325f70ad22e6279d9c1f33
Mattx2k1 <44537080+Mattx2k1@users.noreply.github.com>
2021-10-25 16:20:24 -0400

    testing the test equals method

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -17,23 +17,28 @@ public class JobTest {
     Job testJob1 = new Job();
     Job testJob2 = new Job();
     Job testJob3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+    Job testJob4 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
 
 
     @Test
     public void testSettingJobId() {
         assertEquals(testJob1.getId(), testJob2.getId(), 1);
             System.out.println("IDs match"); // my own identifier test has passed; (Actually this would print anyways I believe =(..)
-
     }
 
     @Test
     public void testJobConstructorSetsAllFields() {
-        assertTrue(testJob3.getName(), Boolean.parseBoolean("Product tester"));
+        assertTrue(testJob3.getName(), Boolean.parseBoolean("Product tester")); // IntelliJ suggested this syntax
         assertTrue(testJob3 instanceof Job);
         assertEquals(testJob3.getEmployer(), "ACME");
         assertEquals(testJob3.getLocation(), "Desert");
         assertEquals(testJob3.getPositionType(), "Quality control");
         assertEquals(testJob3.getCoreCompetency(), "Persistence");
+    }
+
+    @Test
+    public void testJobsForEquality() {
+        assertEquals(testJob3, testJob4);
 
 
     }

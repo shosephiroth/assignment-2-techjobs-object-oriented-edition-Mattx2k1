5f8899959de3cdda4adc090ff3bf0d4c9a7b4634
Mattx2k1 <44537080+Mattx2k1@users.noreply.github.com>
2021-10-25 12:08:05 -0400

    14 tests passing; confused on the JobTest part

---
+++ Tests/JobTest.java
@@ -0,0 +1,15 @@
+
+
+
+@Test
+public void testSettingJobId() {
+    assertEquals(testJob1.getId, testJob2.getId);
+}
+
+@Before
+Job testJob1 = new Job();
+Job testJob2 = new Job();
+
+
+
+
--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,12 +1,26 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.Job;
+
+import static junit.framework.TestCase.assertEquals;
 
 /**
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
 public class JobTest {
+    Job testJob1 = new Job();
+    Job testJob2 = new Job();
+
+
+    @Test
+    public void testSettingJobId() {
+        assertEquals(testJob1, testJob2);
+    }
 
 }
+

aae6ad1783faf4df3619d2ef72335e4c3da36b60
Mattx2k1 <44537080+Mattx2k1@users.noreply.github.com>
2021-10-31 22:34:21 -0400

    Location Extends Jobfield

--- src/main/java/org/launchcode/techjobs/oo/Location.java
+++ src/main/java/org/launchcode/techjobs/oo/Location.java
@@ -2,11 +2,11 @@ package org.launchcode.techjobs.oo;
 
 import java.util.Objects;
 
-public class Location {
+public class Location extends JobField {
 
-    private int id;
-    private static int nextId = 1;
-    private String value;
+//    private int id;
+//    private static int nextId = 1;
+//    private String value;
 
     public Location() {
         id = nextId;
@@ -24,10 +24,10 @@ public class Location {
 
     // Custom toString, equals, and hashCode methods:
 
-    @Override
-    public String toString() {
-        return value;
-    }
+//    @Override
+//    public String toString() {
+//        return value;
+//    }
 
     @Override
     public boolean equals(Object o) {
